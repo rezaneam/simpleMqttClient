a84644f1d685a28c1f83974db941d8723c5cc425
Reza <reza.nimarad@scaleaq.com>
2024-05-02 09:33:53 +0200

    Adding type

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { sensorReading } from './sensorReading'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import {
@@ -57,8 +58,8 @@ function App() {
       });
 
       await connect();
-      attachSignal(hubConnection, "Reading", (client) => {
-        console.log("Reading: ", client);
+      attachSignal(hubConnection, "Reading", (newData: sensorReading) => {
+        console.log("Reading: ", newData);
         AddMessageNo((receivedMessages) => receivedMessages + 1)
       });
 
@@ -76,30 +77,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
 
         <h3>
           Number of recieved Mesasages: {receivedMessages}
         </h3>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ Frontend/src/sensorReading.ts
@@ -0,0 +1,9 @@
+export interface sensorReading {
+  running: boolean;
+  id: number;
+  name: string;
+  speed: number;
+  battery: number;
+  temperature: number;
+  humidity: number;
+}
